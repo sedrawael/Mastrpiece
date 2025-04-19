5b6b995535e03b161cb77ce16bf54486f54a1ad4
abdullahadaileh <abdullahadaileh957@gmail.com>
2025-04-20 00:05:25 +0300

    update users inputs

--- database/migrations/2025_04_01_081529_create_users_table.php
+++ database/migrations/2025_04_01_081529_create_users_table.php
@@ -8,10 +8,9 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role')->default('user'); // أضفنا الدور مباشرة هنا
+            $table->string('role')->default('user'); 
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+        \App\Models\User::create([
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+            }
 }
